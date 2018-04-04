c50e08099779f96c8b5a531ad000bd292424d522
Swackles <Swackles@users.noreply.github.com>
2018-04-04 21:41:52 +0300

    Add project files.

---
+++ PlatformerGame1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PlatformerGame1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateCharacterBtn = new System.Windows.Forms.Button();
+            this.Actions = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CreateCharacterBtn
+            // 
+            this.CreateCharacterBtn.Location = new System.Drawing.Point(13, 13);
+            this.CreateCharacterBtn.Name = "CreateCharacterBtn";
+            this.CreateCharacterBtn.Size = new System.Drawing.Size(100, 23);
+            this.CreateCharacterBtn.TabIndex = 0;
+            this.CreateCharacterBtn.Text = "CreateCharacter";
+            this.CreateCharacterBtn.UseVisualStyleBackColor = true;
+            this.CreateCharacterBtn.Click += new System.EventHandler(this.CreateCharacterBtn_Click);
+            // 
+            // Actions
+            // 
+            this.Actions.Location = new System.Drawing.Point(13, 43);
+            this.Actions.Multiline = true;
+            this.Actions.Name = "Actions";
+            this.Actions.ReadOnly = true;
+            this.Actions.Size = new System.Drawing.Size(100, 406);
+            this.Actions.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.Actions);
+            this.Controls.Add(this.CreateCharacterBtn);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_keyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateCharacterBtn;
+        private System.Windows.Forms.TextBox Actions;
+    }
+}
+
---
+++ PlatformerGame1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlatformerGame1
+{
+    public partial class Form1 : Form
+    {
+        List<Character> CharList = new List<Character>();
+        private Timer ticks;
+
+        public class Character
+        {
+            public int Xpos { set; get; }
+            public int Ypos { set; get; }
+            public int Xhead { set; get; }
+            public int Yhead { set; get; }
+            public bool AI { set; get; }
+            public Graphics graphics { set; get; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region start game
+        private void CreateCharacterBtn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(CharList.Any(x => x.AI == false));
+            if (CharList.Any(x => x.AI == false)) return;
+            Character nobj = new Character();
+
+            nobj.Xpos = this.Height / 2 - 50;
+            nobj.Ypos = this.Width / 2 - 50;
+            nobj.AI = false;
+            nobj.graphics = this.CreateGraphics();
+
+            SolidBrush Brush = new SolidBrush(Color.Red);
+
+            nobj.graphics.FillRectangle(Brush, nobj.Xpos, nobj.Ypos, 50, 50);
+
+            StartTick();
+            CharList.Add(nobj);
+        }
+        #endregion
+
+        #region draw characters
+        public void DrawCharacter(Character obj, int xPos = 0, int yPos = 0)
+        {
+            if (obj.AI)
+            {
+                Character nobj = new Character();
+
+                nobj.graphics.Clear(Color.White);
+
+                nobj.Xpos = obj.Xpos + obj.Xhead;
+                nobj.Ypos = obj.Ypos + obj.Yhead;
+
+                SolidBrush Brush = new SolidBrush(Color.Red);
+                nobj.graphics = this.CreateGraphics();
+                nobj.graphics.FillRectangle(Brush, nobj.Xpos, nobj.Ypos, 50, 50);
+
+                nobj.AI = true;
+                nobj.Xhead = obj.Xhead;
+                nobj.Yhead = obj.Yhead;
+
+                CharList.Remove(obj);
+                CharList.Add(nobj);
+
+            } else
+            {
+                if (obj.Xpos + xPos < 0 || !(obj.Xpos + xPos + 25 < this.Width)) return;
+                if (obj.Ypos + yPos < 0) return;
+
+                Character nobj = new Character();
+
+                obj.graphics.Clear(Color.White);
+
+                nobj.Xpos = obj.Xpos + xPos;
+                nobj.Ypos = obj.Ypos + yPos;
+
+                SolidBrush Brush = new SolidBrush(Color.Red);
+                nobj.graphics = this.CreateGraphics();
+                nobj.graphics.FillRectangle(Brush, nobj.Xpos, nobj.Ypos, 50, 50);
+
+                nobj.AI = false;
+                nobj.Xhead = xPos;
+                nobj.Yhead = yPos;
+
+                CharList.Remove(obj);
+                CharList.Add(nobj);
+            }
+        }
+        #endregion
+
+        #region Main character controlls
+        //move main character
+        private void Form1_keyDown(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine(!(CharList.Any(x => x.AI == false)));
+            if (!(CharList.Any(x => x.AI == false))) return;
+
+            Character obj = CharList.Find(x => x.AI == false);
+
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                DrawCharacter(obj, -5, 0);
+                Actions.Text += "You moved left";
+                Actions.AppendText(Environment.NewLine);
+            }
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                DrawCharacter(obj, 0, -5);
+                Actions.Text += "You moved up";
+                Actions.AppendText(Environment.NewLine);
+            }
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                DrawCharacter(obj, 5, 0);
+                Actions.Text += "You moved right";
+                Actions.AppendText(Environment.NewLine);
+            }
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                DrawCharacter(obj, 0, 5);
+                Actions.Text += "You moved down";
+                Actions.AppendText(Environment.NewLine);
+            }
+        }
+        #endregion
+
+        #region create AI
+        private void CreateAi()
+        {
+
+        }
+        #endregion
+
+
+        #region ticks
+        private void StartTick()
+        {
+            ticks = new Timer();
+            ticks.Tick += new EventHandler(Tick);
+            ticks.Interval = 17;
+            ticks.Start();
+
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            Actions.Text += "Tick";
+            Actions.AppendText(Environment.NewLine);
+
+            for (int i = 0; i < CharList.Count(); i++)
+            {
+                if (!(CharList[i].AI)) continue;
+                DrawCharacter(CharList[i]);
+            }
+        }
+        #endregion
+    }
+}
